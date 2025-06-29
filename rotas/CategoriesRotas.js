1eb09b43d6a04ef6859c96fa94d7bcd58144fc84
D-mi5 <deynemichelles@gmail.com>
2025-06-29 16:28:00 -0300

    Adiciona rotas de categorias

--- rotas/CategoriesRotas.js
+++ rotas/CategoriesRotas.js
@@ -2,4 +2,18 @@ const express = require('express');
 const CategoriesController = require('../controllers/CategoriesController');
 const CategoriesRotas = express.Router();
 
+
+
+const controller = new CategoriesController();
+
+CategoriesRotas.get('/v1/category/search', controller.buscar);
+
+CategoriesRotas.get('//v1/category/:id', controller.consultarPorId);
+
+CategoriesRotas.post('/v1/category', controller.criar)
+
+CategoriesRotas.put('/v1/category/:id', controller.atualizar);
+
+CategoriesRotas.delete('/v1/category/:id',  controller.deletar);
+
 module.exports = CategoriesRotas;
